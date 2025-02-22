f468d8e369de6cbaee5712c4319971e6f15e667b
Aleks247 <aleks247bg@gmail.com>
2025-02-22 12:40:25 +0200

    Revert "Creating change email and password methods and endpoints"
    This reverts commit c09dd671906dde7d2c42de0c90e1765ddd632bd4.

--- src/main/java/com/obelix/homework/platform/controller/UserController.java
+++ src/main/java/com/obelix/homework/platform/controller/UserController.java
@@ -2,30 +2,11 @@ package com.obelix.homework.platform.controller;
 
 import com.obelix.homework.platform.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
-
     private final UserService userService;
-
-//    @GetMapping("/exists/{username}")
-//    public ResponseEntity<Boolean> checkUsernameExists(@PathVariable String username) {
-//        return ResponseEntity.ok(userService.existsByUsername(username));
-//    }
-
-    @PostMapping("/change-password")
-    public ResponseEntity<String> changePassword(@RequestParam String oldPassword, @RequestParam String newPassword) {
-        userService.changePassword(oldPassword, newPassword);
-        return ResponseEntity.ok("Password changed successfully");
-    }
-
-    @PostMapping("/change-email")
-    public ResponseEntity<String> changeEmail(@RequestParam String newEmail) {
-        userService.changeEmail(newEmail);
-        return ResponseEntity.ok("Email changed successfully");
-    }
+        //logic for universal user logic like changing email, password, username, so on
 }
