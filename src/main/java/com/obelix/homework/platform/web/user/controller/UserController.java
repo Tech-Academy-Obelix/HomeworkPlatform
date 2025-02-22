2be0b3eb66b921d53497b02ae58c6288c9eddf42
Aleks247 <aleks247bg@gmail.com>
2025-02-22 23:18:13 +0200

    Restructure

--- src/main/java/com/obelix/homework/platform/web/user/controller/UserController.java
+++ src/main/java/com/obelix/homework/platform/web/user/controller/UserController.java
@@ -2,11 +2,53 @@ package com.obelix.homework.platform.web.user.controller;
 
 import com.obelix.homework.platform.web.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+
     private final UserService userService;
-        //logic for universal user logic like changing email, password, username, so on
+
+//    @GetMapping("/exists/{username}")
+//    public ResponseEntity<Boolean> checkUsernameExists(@PathVariable String username) {
+//        return ResponseEntity.ok(userService.existsByUsername(username));
+//    }
+
+    @PostMapping("/change-password")
+    public ResponseEntity<String> changePassword(@RequestBody Map<String, String> payload) {
+        String oldPassword = payload.get("oldPassword");
+        String newPassword = payload.get("newPassword");
+        userService.changePassword(oldPassword, newPassword);
+        return ResponseEntity.ok("Password changed successfully");
+    }
+
+    @PostMapping("/change-email")
+    public ResponseEntity<String> changeEmail(@RequestBody Map<String, String> payload) {
+        userService.changeEmail(payload.get("newEmail"));
+        return ResponseEntity.ok("Email changed successfully");
+    }
+
+    @PostMapping("/change-username")
+    public ResponseEntity<String> changeUsername(@RequestBody Map<String, String> payload) {
+        userService.changeUsername(payload.get("newUsername"));
+        return ResponseEntity.ok("Username changed successfully");
+    }
+
+    @PostMapping("/change-firstname")
+    public ResponseEntity<String> changeFirstName(@RequestBody Map<String, String> payload) {
+        userService.changeFirstName(payload.get("newFirstName"));
+        return ResponseEntity.ok("First name changed successfully");
+    }
+
+    @PostMapping("/change-lastname")
+    public ResponseEntity<String> changeLastName(@RequestBody Map<String, String> payload) {
+        userService.changeLastName(payload.get("newLastName"));
+        return ResponseEntity.ok("Last name changed successfully");
+    }
 }
